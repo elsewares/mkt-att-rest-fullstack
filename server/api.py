4aff6e8b09dde748fba22b65343c7df7fc921f0e
Brian Hollenbeck <brian@elsewares.org>
2018-01-23 21:33:41 -0600

    Various logical improvements.

--- server/api.py
+++ server/api.py
@@ -18,34 +18,30 @@ def get():
 
 
 # SCHEMA: user = {username: <string>}
-@app.route('/user/<string:username>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/user/<string:username>', methods=['GET', 'POST', 'DELETE'])
 def dispatch(username):
     if request.method == 'GET':
         return mongo.db.users.find_one_or_404({'username': username})
     elif request.method == 'POST':
         if not mongo.db.users.find_one({'username': username}): #check for an existing username.
-            return mongo.db.repos.update({'username': username, $push: { 'name': request.data.repo_name }})
+            return mongo.db.repos.create({'username': username})
         else:
-            return make_request('Repo name already taken.', 400)
-    elif request.method == 'PUT':
-        return mongo.db.users.update_one({'username': username}, request.data)
+            return make_request('User name already taken.', 400)
     elif request.method == 'DELETE':
         return mongo.db.users.delete_one({'username': username})
 
 
 # SCHEMA: repo = {username: <string>, name: <string>}
-@app.route('/repos/<string:username>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/repos/<string:username>', methods=['GET', 'POST', 'DELETE'])
 def dispatch(username):
     if request.method == 'GET':
         return mongo.db.repos.find_one_or_404({'username': username})
     elif request.method == 'POST':
         #check to make sure that the repo name is unique.
         if not mongo.db.repos.find_one({'username': username, 'name': { $in: [request.data.repo_name]}}):
-            return mongo.db.repos.update({'username': username, $push: { 'name': request.data.repo_name }})
+            return mongo.db.repos.update({'username': username}, $push: { 'name': request.data.repo_name }})
         else:
             return make_request('Repo name already taken.', 400)
-    elif request.method == 'PUT':
-        return mongo.db.repos.update_one({'username': username}, request.data)
     elif request.method == 'DELETE':
         return mongo.db.repos.delete_one({'username': username})
 
